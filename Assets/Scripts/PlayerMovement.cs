0685fe585fd22c02f5cff3279479c75e24d5e62e
Andrew McCauley <developer@littleagames.com>
2020-08-01 10:10:43 -0500

    Initial project + scene building + player movement and collision detection

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{ 
+    public float MovementSpeed = 20f;
+    public float DiagonalLimiter = 0.7f;
+
+    private Rigidbody2D _rigidbody2d;
+
+    private float _horizMovement;
+
+    private float _vertMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _horizMovement = Input.GetAxis("Horizontal");
+        _vertMovement = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (_horizMovement != 0f && _vertMovement != 0f) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            _horizMovement *= DiagonalLimiter;
+            _vertMovement *= DiagonalLimiter;
+        }
+
+        _rigidbody2d.velocity = new Vector2(_horizMovement * MovementSpeed, _vertMovement * MovementSpeed);
+    }
+}
