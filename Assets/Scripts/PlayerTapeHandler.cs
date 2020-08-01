d3966f861946856fe6658f98b858190b3adc2b9c
Andrew McCauley <developer@littleagames.com>
2020-08-01 10:26:24 -0500

    Adding video tape, and collision handling between player and tape, and player and rewinder

---
+++ Assets/Scripts/PlayerTapeHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTapeHandler : MonoBehaviour
+{
+    private GameObject _tapeOnHand = null;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Tape")
+        {
+            TryCollectTape(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Rewinder")
+        {
+            Destroy(_tapeOnHand);
+            _tapeOnHand = null;
+        }
+    }
+
+    private void TryCollectTape(GameObject tapeObject)
+    {
+        if (_tapeOnHand != null)
+        {
+            return;
+        }
+        else
+        {
+            tapeObject.transform.parent = transform;
+            _tapeOnHand = tapeObject;
+        }
+    }
+}
